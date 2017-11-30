ed973c5cf0f4eee546063443617307b4762e0588
korgogro <korgogro@gmail.com>
2017-11-30 17:28:43 +0100

    Initial commit
    VCV Rack plugins empty structure

---
+++ src/NonLinearInstruments.cpp
@@ -0,0 +1,22 @@
+#include "NonLinearInstruments.hpp"
+
+
+// The plugin-wide instance of the Plugin class
+Plugin *plugin;
+
+void init(rack::Plugin *p) {
+	plugin = p;
+	// This is the unique identifier for your plugin
+	p->slug = "NonLinearInstruments";
+#ifdef VERSION
+	p->version = TOSTRING(VERSION);
+#endif
+	p->website = "https://github.com/VCVRack/Tutorial";
+	p->manual = "https://github.com/VCVRack/Tutorial/blob/master/README.md";
+
+	// For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
+	p->addModel(createModel<QU4DiTWidget>("NonLinearInstruments", "QUADiT", "Quadratic Iterator", OSCILLATOR_TAG));
+
+	// Any other plugin initialization may go here.
+	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
+}
---
+++ src/NonLinearInstruments.hpp
@@ -0,0 +1,15 @@
+#include "rack.hpp"
+
+
+using namespace rack;
+
+
+extern Plugin *plugin;
+
+////////////////////
+// module widgets
+////////////////////
+
+struct QU4DiTWidget : ModuleWidget {
+	QU4DiTWidget();
+};
